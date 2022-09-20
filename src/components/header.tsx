7fb684d990ac79c21e2fcf78f75f7e5fc99a1a87
tak n <tkn.infomail@gmail.com>
2022-09-20 23:55:07 +0900

    Update privacy

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,6 +15,7 @@ const Header = () => {
   const navItems: NavItem[] = [
     { label: 'Home', path: '/' },
     { label: 'Blog', path: '/blog' },
+    { label: 'Privacy', path: '/privacy' },
   ]
 
   return (
