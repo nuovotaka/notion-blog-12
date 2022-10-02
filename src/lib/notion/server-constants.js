fcb7cdaf7cef7a241318fbbdbe6cad673898fbcb
tak n <tkn.infomail@gmail.com>
2022-10-02 09:50:18 +0900

    add google maps embed

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -8,6 +8,7 @@ const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const NEXT_PUBLIC_G_ADS_ID = process.env.NEXT_PUBLIC_G_ADS_ID
+const NEXT_PUBLIC_GCMaps_API_KEY = process.env.NEXT_PUBLIC_GCMaps_API_KEY
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
@@ -17,6 +18,7 @@ module.exports = {
   NEXT_PUBLIC_URL,
   NEXT_PUBLIC_GA_TRACKING_ID,
   NEXT_PUBLIC_G_ADS_ID,
+  NEXT_PUBLIC_GCMaps_API_KEY,
   BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
