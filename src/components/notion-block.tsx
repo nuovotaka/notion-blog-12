d849a1d319bca9bd7b30314842427943484a3f09
tak n <tkn.infomail@gmail.com>
2022-10-03 04:04:45 +0900

    Update ToDo

--- src/components/notion-block.tsx
+++ src/components/notion-block.tsx
@@ -296,7 +296,7 @@ const ToDoItems = ({ blocks }) =>
     .filter((b: interfaces.Block) => b.Type === 'to_do')
     .map((listItem: interfaces.Block) => (
       <div key={`to-do-item-${listItem.Id}`}>
-        <input type="checkbox" checked={listItem.ToDo.Checked} />
+        <input type="checkbox" readOnly checked={listItem.ToDo.Checked} />
         <span>
         {listItem.ToDo.RichTexts.map((richText: interfaces.RichText, i: number) => (
           <RichText
