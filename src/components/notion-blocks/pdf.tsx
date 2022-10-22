41b8a086e50a21ebaba6f475ef0cc525b31896f1
tak n <tkn.infomail@gmail.com>
2022-10-23 07:39:20 +0900

    Updata pdf

--- src/components/notion-blocks/pdf.tsx
+++ src/components/notion-blocks/pdf.tsx
@@ -4,29 +4,32 @@ import React, { useEffect, useState } from 'react'
 import styles from '../../styles/notion-block.module.css'
 
 const Pdf = ({ block }) => {
-
   const url = new URL(block.Pdf.File.Url).href
   const [pdfdata, setPdfdata] = useState(null)
+  
+  if (/^https:\/\/s3\.us-west-2\.amazonaws\.com/.test(block.Pdf.File.Url)) {
 
-  useEffect(() => {
-    try {
-      axios.get(url, {
-        headers: {contentType: 'application/pdf'},
-        responseType: 'blob',
-      })
-      .then(res => {
-        const blob = new Blob([res.data],{ type:'application/pdf'})
-        const url = window.URL || window.webkitURL
-        const src = url.createObjectURL(blob)
-        setPdfdata(src)
-      })
-    } catch (e) {
-      console.log(e.message)
-    }
-  }, [url])
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      try {
+        axios.get(url, {
+          headers: {contentType: 'application/pdf'},
+          responseType: 'blob',
+        })
+        .then(res => {
+          const blob = new Blob([res.data],{ type:'application/pdf'})
+          const url = window.URL || window.webkitURL
+          const src = url.createObjectURL(blob)
+          setPdfdata(src)
+        })
+      } catch (e) {
+        console.log(e.message)
+      }
+    }, [url])
 
-  console.log(pdfdata)
-  if(!pdfdata) return null
+    console.log(pdfdata)
+    if(!pdfdata) return null
+  } else { return null }
 
   return (
     <div className={styles.pdf}>
