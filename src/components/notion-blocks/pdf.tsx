b53d6da103664ce73ff090ce32c4be6cc970d92f
tak n <tkn.infomail@gmail.com>
2022-11-01 00:48:51 +0900

    Update pdf embed

--- src/components/notion-blocks/pdf.tsx
+++ src/components/notion-blocks/pdf.tsx
@@ -6,34 +6,28 @@ import styles from '../../styles/notion-block.module.css'
 const Pdf = ({ block }) => {
   const url = new URL(block.Pdf.File.Url).href
   const [pdfdata, setPdfdata] = useState(null)
-  
-  if (/^https:\/\/s3\.us-west-2\.amazonaws\.com/.test(block.Pdf.File.Url)) {
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      try {
-        axios.get(url, {
-          headers: {contentType: 'application/pdf'},
-          responseType: 'arraybuffer',
-        })
-        .then(res => {
-          const blob = new Blob([res.data],{ type:'application/pdf'})
-          const url = window.URL || window.webkitURL
-          const src = url.createObjectURL(blob)
-          setPdfdata(src)
-        })
-      } catch (e) {
-        console.log(e.message)
-      }
-    }, [url])
+  useEffect(() => {
+    try {
+      axios.get(url, {
+        headers: {contentType: 'application/pdf'},
+        responseType: 'arraybuffer',
+      })
+      .then(res => {
+        const blob = new Blob([res.data],{ type:'application/pdf'})
+        const src = URL.createObjectURL(blob)
+        setPdfdata(src)
+      })
+    } catch (e) {
+      console.log(e.message)
+    }
+  }, [url])
 
-    console.log(pdfdata)
-    if(!pdfdata) return null
-  } else { return null }
+  if(!pdfdata) return null
 
   return (
     <div className={styles.pdf}>
-
+      <iframe src={pdfdata} width='100%' height='100%'></iframe>
     </div>
   )
 }
