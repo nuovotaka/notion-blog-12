c0cbb311f38bee1c46abce009207a0827b3b65a6
tak n <tkn.infomail@gmail.com>
2022-11-03 00:18:48 +0900

    fix codepen

--- src/components/notion-blocks/codepen.tsx
+++ src/components/notion-blocks/codepen.tsx
@@ -4,23 +4,21 @@ import Codepen from 'react-codepen-embed'
 import styles from '../../styles/notion-block.module.css'
 
 const CodepenEmbedded = ({ url }) => {
-	let username
-	let hash
+  let matched: string[]
   try {
-    username = new URL(url).pathname.match(/^\/(.+)\/pen/)
-		hash = new URL(url).pathname.match(/pen\/(.+)$/)
+    matched = new URL(url).pathname.match(/^\/(.+)\/pen\/(.+)$/)
   } catch (error) {
     console.log(error)
     return null
   }
 
-  if (!username) {
+  if (!matched) {
     return null
   }
 
   return (
 		<div className={styles.codepenembed}>
-			<Codepen hash={hash[1]} user={username[1]} />
+			<Codepen hash={matched[2]} user={matched[1]} />
 		</div>
 	)
 }
