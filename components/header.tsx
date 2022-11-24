032989e3be8caeb95ed72cb81cc7ff1641ba7476
tak n <tkn.infomail@gmail.com>
2022-11-25 04:23:47 +0900

    Define Header as a client component

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,13 @@
-import dynamic from 'next/dynamic'
+'use client'
+
+import { usePathname } from "next/navigation"
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { NEXT_PUBLIC_SITE_TITLE } from '../app/server-constants'
+import ModeSwitch from './mode-switch'
 import { FaHome, FaLink, FaUser } from 'react-icons/fa'
 import { MdPrivacyTip } from 'react-icons/md'
 
 import styles from '../styles/header.module.scss'
-
-const ModeSwitch = dynamic(() => import('./mode-switch'))
 
 interface NavItem {
   label: string
@@ -23,7 +23,7 @@ interface ItemCSS extends React.CSSProperties {
 }
 
 const Header = () => {
-  const { asPath } = useRouter()
+  const pathname = usePathname()
 
   const navItems: NavItem[] = [
     { label: 'Home', path: '/' , icon: <FaHome color={'#999'} />, color1: '#ff9966', color2: '#ff5e62' },
@@ -35,8 +35,8 @@ const Header = () => {
   return (
     <>
       <h1 className={styles.siteheader}>
-        <Link href="/" passHref>
-          <a>{NEXT_PUBLIC_SITE_TITLE}</a>
+        <Link href="/">
+          {NEXT_PUBLIC_SITE_TITLE}
         </Link>
       </h1>
       <header className={styles.header}>
@@ -45,8 +45,8 @@ const Header = () => {
             <li style={{'--i': color1, '--j': color2} as ItemCSS} key={label} >
               <span className={styles.icon}>{icon}</span>
               <span className={styles.title}>
-                <Link href={path} passHref>
-                  <a className={asPath === path ? 'active' : null}>{label}</a>
+                <Link href={path} className={pathname === path ? 'active' : null}>
+                  {label}
                 </Link>
               </span>
             </li>
