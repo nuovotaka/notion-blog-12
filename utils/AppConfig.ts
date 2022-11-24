68473863d5129223ad929034533c006a305e0bb6
tak n <tkn.infomail@gmail.com>
2022-11-24 13:17:40 +0900

    add test Like

--- utils/AppConfig.ts
+++
@@ -1,9 +0,0 @@
-export const AppConfig = {
-  site_name: 'dev.NUOVOTAKA',
-  title: 'dev.NUOVOTAKA',
-  description:
-    '時々更新ブログ',
-  url: 'https://blog.nuovotaka.com',
-  locale: 'ja',
-  author: 'NUOVOTAKA',
-};
