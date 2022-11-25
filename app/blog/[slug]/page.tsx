ea9e9d38c48cd5056e5a6b63fde4d4043978b02d
tak n <tkn.infomail@gmail.com>
2022-11-25 14:07:24 +0900

    fix blog style

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -60,7 +60,7 @@ const BlogSlugPage = async ({ params: { slug } }) => {
   return (
     <div className={styles.container}>
       <div className={styles.mainContent}>
-        <div className={styles.post}>
+        <div className={styles.blogpost}>
           <PostDate post={post} />
           <PostTags post={post} />
           <PostTitle post={post} enableLink={false} />
