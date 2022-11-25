24bafc0cbf8d90fcb01ee944c2a9a00944c93f27
tak n <tkn.infomail@gmail.com>
2022-11-25 20:20:02 +0900

    Remove Loading bar client

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,21 +8,15 @@ import styles from '../styles/shared.module.scss'
 import GoogleAnalytics from '../components/google-analytics'
 import GoogleADS from '../components/google-ads'
 import { NEXT_PUBLIC_SITE_TITLE } from './server-constants'
-import { useState } from 'react'
-import LoadingBar from 'react-top-loading-bar'
 
 const RootLayout = ({
   children,
 }: {
   children: React.ReactNode,
 }) => {
-
-  const [progress,setProgress] = useState(0);
-  const barColor = '#0fa8ff'
   return (
   <html lang="ja">
     <body>
-      <LoadingBar color={barColor} progress={progress} onLoaderFinished={() => setProgress(0)} />
       <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
       <GoogleADS />
       <div className={styles.container}>
